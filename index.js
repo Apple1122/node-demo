4cf9769262c14597f26616e6779e9c6736b26496
Unknown <kyle.huang168@gmail.com>
2018-05-14 21:07:17 +0800

    crawer

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const Xray = require('x-ray');
+const x = Xray();
+
+x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html', 'table > tr', [{
+    No: 'td:nth-child(2)',
+    courseName: 'td:nth-child(4)',
+    teacher: 'td:nth-child(5)'
+}]).write('./dist/course.json');
+
+
 console.log('hello\n');  //TODO:
-                       //FIXME:
+                         //FIXME:
